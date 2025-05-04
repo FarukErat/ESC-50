10653245c375fdb683b9f05afd06158dc498b5e4
FarukErat <farukerat1327@gmail.com>
2025-05-04 14:46:10 +0300

    update csv

--- scripts/0.rename_audio_files.py
+++ scripts/0.rename_audio_files.py
@@ -3,9 +3,12 @@ import csv
 
 csv_file = 'meta/esc50.csv'
 audio_folder = 'audio'
+updated_rows = []
 
 with open(csv_file, newline='') as f:
     reader = csv.DictReader(f)
+    fieldnames = reader.fieldnames
+
     for row in reader:
         original_filename = row['filename']
         category = row['category']
@@ -19,3 +22,11 @@ with open(csv_file, newline='') as f:
             print(f"Renamed: {original_filename} → {new_filename}")
         else:
             print(f"File not found: {original_filename}")
+
+        row['filename'] = new_filename
+        updated_rows.append(row)
+
+with open(csv_file, 'w', newline='') as f:
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(updated_rows)
